c198affbbc6d20209a09be0e45ccb771cd3c2141
Matthias Fechner <idefix@fechner.net>
2017-03-17 17:22:12 +0100

    On first run it is displayed, every reload does not show anything. Closing Chrome completely and restart it, displays the tree once. Each reload does not show it anymore.

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -5,64 +5,64 @@ import { TreeNode } from "angular2-tree-component";
 import { CategoriesDataService } from "./categories/categories.service";
 
 @Component({
-  selector: "app",
-  template,
-  styles: [ style ]
+    selector: "app",
+    template,
+    styles: [ style ]
 })
 export class AppComponent implements OnInit, OnDestroy {
-  nodes = [];
+    nodes = [];
 
-  options;
-  nodesSubscription;
+    options;
+    nodesSubscription;
 
-  constructor(private categoriesDataService: CategoriesDataService) {
-    this.options = {
-      getChildren: this.getChildren.bind(this),
-      //idField: "id",
-      //displayField: "id"
-    };
+    constructor(private categoriesDataService: CategoriesDataService) {
+        this.options = {
+            getChildren: this.getChildren.bind(this),
+        };
 
-  }
+    }
 
     getChildren (node: TreeNode) {
-        console.log("Called getChildren with", node);
-/*
-        return this.categoriesDataService.getData(node.childrenIds)
-        .subscribe(res => {
-            console.log("In data subscribe");
-            let children = [];
-            for (let r of res) {
-                console.log("Child in loop", r);
-                children.push(r);
-            }
-        });
-        */
+        console.log("getChildren: Called with", node);
+
+        return this.categoriesDataService.getData(node.data.childrenIds)
+            .subscribe(res => {
+                console.log("getChildren: In data subscribe");
+                let children = [];
+                for (let r of res) {
+                    console.log("getChildren: Child in loop", r);
+                    children.push(this.toNode(r));
+                }
+                console.log("getChildren: after loop", children);
+                return children;
+            });
     }
 
 
-  ngOnInit() {
-    console.log("In app.component ngOnInit");
-    this.nodesSubscription = this.categoriesDataService.getData()
-      .subscribe(res => {
-        console.log("ngOnInit Subscription", res);
-        this.nodes.push(this.toNode(res[0]));
-        //  this.nodes.push({id: "1", name: "Test"});
-        console.log("Transformed nodes to", this.nodes);
-      });
-    console.log("Finished ngOnInit");
-  }
+    ngOnInit() {
+        console.log("In app.component ngOnInit");
+        this.nodesSubscription = this.categoriesDataService.getData()
+            .subscribe(res => {
+                console.log("ngOnInit Subscription", res);
+                this.nodes.push(this.toNode(res[0]));
+                console.log("Transformed nodes to", this.nodes);
+            });
+        console.log("Finished ngOnInit");
+    }
 
-  ngOnDestroy() {
-    this.nodesSubscription.unsubscribe();
-  }
+    ngOnDestroy() {
+        this.nodesSubscription.unsubscribe();
+    }
 
-  toNode(dataset) {
-      console.log("toNode with ", dataset);
-      console.log("Length: ", dataset.children.length);
-      return {
-          id: dataset._id,
-          name: dataset._id,
-          hasChildren: dataset.children.length
-      };
-  }
+    toNode(dataset) {
+        console.log("toNode with ", dataset);
+        console.log("Length: ", dataset.children.length);
+        return {
+            id: dataset._id,
+            name: dataset._id,
+            hasChildren: dataset.children.length > 0 ? true : false,
+            childrenIds: dataset.children,
+            children: []
+        };
+    }
 }
